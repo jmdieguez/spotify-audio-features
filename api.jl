23ae00b003a9a842aa0c9b29d573f693c5a3b7fd
jdieguez1 <jdieguez@fi.uba.ar>
2024-01-02 18:17:21 -0300

    Esqueleto api

---
+++ api.jl
@@ -0,0 +1,55 @@
+import Pkg
+
+Pkg.add("Oxygen")
+
+using Oxygen, HTTP, JSON
+
+SPOTIFY_API_URL = "https://api.spotify.com/v1/"
+
+function api_http_get_request(endpoint, headers)
+	url = SPOTIFY_API_URL * endpoint
+	response = HTTP.get(url, headers)
+	if response.status == 200
+		return JSON.parse(String(response.body))
+	else
+	    println("Error al obtener los datos de $endpoint")
+		return nothing
+	end
+end
+
+function encode_ids(ids, headers)
+	return join(map(x -> replace(x, "/" => "%2F", "+" => "%2B", " " => "%20"), ids), ",")
+end
+
+function get_track_info(track_id, headers)
+    return api_http_get_request("tracks/$track_id", headers)
+end
+
+function get_track_audio_features(track_id, headers)
+    return api_http_get_request("audio-features/$track_id", headers)
+end
+
+function get_favorite_tracks(headers)
+    return api_http_get_request("me/top/tracks?limit=50", headers)
+end
+
+function get_audio_features_multiple_tracks(ids, headers)
+    return api_http_get_request("audio-features?ids=$(encode_ids(ids))", headers)
+end
+
+function get_recommendations(ids, headers)
+    return api_http_get_request("recommendations?seed_tracks=$(encode_ids(ids))&limit=100", headers)["tracks"]
+end
+
+@get "/fav" function(req::HTTP.Request)
+    token = queryparams(req)["token"]
+
+    headers = Dict(
+			"Authorization" => "Bearer $token",
+			"Content-Type" => "application/json"
+		    )
+
+    return get_favorite_tracks(headers)
+end
+
+serve()
